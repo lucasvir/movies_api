1b66be3ba82002b8baaaffe862e79f80a7093398
lucasvir <lavirmond@gmail.com>
2023-03-06 10:54:30 -0300

    adding pm2 for deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+require('dotenv/config');
 
 const express = require('express');
 const routes = require('./routes');
@@ -32,7 +33,7 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 5555;
+const PORT = process.env.PORT || 5555;
 app.listen(PORT, () =>
   console.log(`Server is running on PORT: ${PORT}`)
 );
